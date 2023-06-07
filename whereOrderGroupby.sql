679d7288142364bd6579c96d4f0ad0bb4aa20dd8
estskyway <estskyway@gmail.com>
2023-06-07 17:34:59 +0900

    6/7 17시 34분

--- whereOrderGroupby.sql
+++ whereOrderGroupby.sql
@@ -2,8 +2,9 @@ SELECT COUNT(*) FROM ORDERS
 WHERE EmployeeID IN (7,9)
 GROUP BY EmployeeID; 
 
--- COUNT(*)
--- 14
--- 6
+
+-- COUNT(EmployeeID)	EmployeeID
+-- 14					7
+-- 6					9
 
 -- REFER : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_min
