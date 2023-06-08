6f3ac37e4b1694223f714ab076dd02dabd403075
estskyway <estskyway@gmail.com>
2023-06-08 13:06:05 +0900

    6/8 13시 5분

---
+++ CustomersJoinOrders.sql
@@ -0,0 +1,27 @@
+-- PK는 CustomerID
+SELECT T_CUSTOMERS.CustomerName, COUNT(T_ORDERS.OrderID)
+	FROM Customers AS T_CUSTOMERS
+	INNER JOIN ORDERS AS T_ORDERS
+    ON T_CUSTOMERS.CustomerID = T_ORDERS.CustomerID
+GROUP BY T_CUSTOMERS.CustomerName;
+
+
+-- Number of Records: 74
+
+-- CustomerName	COUNT(T_ORDERS.OrderID)
+-- Ana Trujillo Emparedados y helados	1
+-- Antonio Moreno Taquería	1
+-- Around the Horn	2
+-- B's Beverages	1
+-- Berglunds snabbköp	3
+-- Blondel père et fils	4
+-- Bon app'	3
+-- Bottom-Dollar Marketse	4
+-- Bólido Comidas preparadas	1
+-- Centro comercial Moctezuma	1
+-- .
+-- .
+-- .
+-- 이하생략
+
+-- REFER : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_columns
