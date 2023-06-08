ecc6d51fbecef9ce00be53b315d2c8abd446c8f2
estskyway <estskyway@gmail.com>
2023-06-08 13:11:09 +0900

    6/8 13시 11분

--- CustomersJoinOrders.sql
+++ CustomersJoinOrders.sql
@@ -1,5 +1,5 @@
 -- PK는 CustomerID
-SELECT T_CUSTOMERS.CustomerName, COUNT(T_ORDERS.OrderID)
+SELECT T_CUSTOMERS.CustomerName, COUNT(T_ORDERS.OrderID) AS T_ORDERS_CNT
 	FROM Customers AS T_CUSTOMERS
 	INNER JOIN ORDERS AS T_ORDERS
     ON T_CUSTOMERS.CustomerID = T_ORDERS.CustomerID
@@ -8,17 +8,17 @@ GROUP BY T_CUSTOMERS.CustomerName;
 
 -- Number of Records: 74
 
--- CustomerName	COUNT(T_ORDERS.OrderID)
--- Ana Trujillo Emparedados y helados	1
--- Antonio Moreno Taquería	1
--- Around the Horn	2
--- B's Beverages	1
--- Berglunds snabbköp	3
--- Blondel père et fils	4
--- Bon app'	3
--- Bottom-Dollar Marketse	4
--- Bólido Comidas preparadas	1
--- Centro comercial Moctezuma	1
+CustomerName	T_ORDERS_CNT
+Ana Trujillo Emparedados y helados	1
+Antonio Moreno Taquería	1
+Around the Horn	2
+B's Beverages	1
+Berglunds snabbköp	3
+Blondel père et fils	4
+Bon app'	3
+Bottom-Dollar Marketse	4
+Bólido Comidas preparadas	1
+Centro comercial Moctezuma	1
 -- .
 -- .
 -- .
