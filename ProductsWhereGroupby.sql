dbb27cc0b6aa7b90530e4cab03f0af3575fd7bde
estskyway <estskyway@gmail.com>
2023-06-08 11:09:37 +0900

    6/8 11시 9분

--- ProductsWhereGroupby.sql
+++ ProductsWhereGroupby.sql
@@ -1,7 +1,15 @@
-SELECT COUNT(T_PRO.CategoryID) AS CNT_CATE_ID 
+SELECT COUNT(T_PRO.CategoryID) AS CNT_CATE_ID, CategoryID
 FROM Products AS T_PRO
 GROUP BY T_PRO.CategoryID
 HAVING COUNT(T_PRO.CategoryID) >= 10;
 
 -- 원하는 답이 안나와요 .... 무엇이 잘못 되었는지 모르겠어요 
+-- Number of Records: 5
+
+-- CNT_CATE_ID	CategoryID
+-- 12		1
+-- 12		2
+-- 13		3
+-- 10		4
+-- 12		8
 -- REFER : https://www.w3schools.com/sql/trysql.asp?filename=trysql_select_having
